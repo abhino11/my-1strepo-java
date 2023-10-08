5ceaf89f6b4be87b608a263d92216cc2b2d2b8f6
abhishek meena <82045366+abhixo114@users.noreply.github.com>
2023-10-08 15:59:54 +0530

    number java add
    2nd program in java

---
+++ numbersInJava2.java
@@ -0,0 +1,18 @@
+class number{
+
+public static void main(String []args){
+
+byte a=105;
+System.out.println(a);
+
+int b=2023;
+System.out.println(b);
+
+long c=123456789;
+System.out.println(c);
+
+short e=12;
+System.out.println(e);
+
+}
+}
